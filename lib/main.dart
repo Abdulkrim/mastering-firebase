93542d809fd64c09ac634e78026583d420ce9203
a.salem <abdulkarimsalem1997@gmail.com>
2025-01-01 21:08:47 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttercourse/auth/login.dart';
+import 'package:fluttercourse/auth/signup.dart';
+import 'package:fluttercourse/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    FirebaseAuth.instance
+        .authStateChanges()
+        .listen((User? user) {
+      if (user == null) {
+        print('==========================User is currently signed out!');
+      } else {
+        print('==========================User is signed in!');
+      }
+    });
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirebaseAuth.instance
+          .currentUser == null ? Login() : Homepage(),
+      routes: {
+        "signup" : (context) => SignUp() ,
+        "login" : (context) => Login(),
+        "homepage" : (context) => Homepage(),
+      },
+    );
+  }
+}
